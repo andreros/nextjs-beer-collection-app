87c852221780f886f2c0975dc1e4a854b1c99c04
André Rosa <andreros@gmail.com>
2023-11-20 10:13:00 +0000

    feat: added Next.JS 14 base application and configuration to the project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { getEnvironmentVariables } from '@/tools/tools';
+import packageJson from '@/package.json';
+import './globals.scss';
+
+export const metadata: Metadata = {
+    title: getEnvironmentVariables('applicationName'),
+    description: packageJson.description,
+    authors: {
+        name: packageJson.author
+    },
+    other: {
+        version: packageJson.version,
+        license: packageJson.license
+    },
+    icons: './favicon.ico'
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function RootPage() {
+    return <main>Beer Collection App</main>;
+}
